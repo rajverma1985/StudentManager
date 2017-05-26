d9c8902c2b5c97ea400b563f39466bea1aff39db
Raj Verma <rjnshvrm23@gmail.com>
2017-05-26 07:34:26 -0700

    add, print and get students code

--- functions.py
+++ functions.py
@@ -1,3 +1,26 @@
+students=[]
+
+def get_students():
+    student_name=[]
+    for student in students:
+        student_name=student["student_name"].title()
+        student_id = student["student_id"].title()
+        return student_name , student_id
 
 
+def print_students():
+    print_s = get_students()
+    print(print_s)
 
+
+def add_students(name, id):
+    student = {"student_name": name, "student_id": id}
+    students.append(student)
+
+student_list = get_students()
+
+student_name = input("Enter a name : ")
+student_id = input("Enter an id : ")
+
+add_students(student_name, student_id)
+print_students()
